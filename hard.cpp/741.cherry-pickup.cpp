09d45ae3357578198fa374121da0610047ae29bb
LinkeyLeo <LinkeyLeo@outlook.com>
2020-03-09 01:00:36 +0800

    Update 741.cherry-pickup.cpp
    741: Mor impovement, but maybe wrong direction
    /
    24/34/9/74.5%
    CS[1/2/0/38%]

--- hard.cpp/741.cherry-pickup.cpp
+++ hard.cpp/741.cherry-pickup.cpp
@@ -9,14 +9,20 @@ class Solution
 {
 private:
     map<unsigned int, int> cache;
-    void cherryPickup(vector<vector<int>> &grid, vector<int> p0, vector<int> p1, unsigned int location)
+    int tolerance = INT_MAX;
+    void cherryPickup(vector<vector<int>> &grid, vector<int> p0, vector<int> p1, unsigned int location, int waste)
     {
+        if (waste > tolerance)
+        {
+            return;
+        }
+        waste += 2 - grid[p0[0]][p0[1]] - grid[p1[0]][p1[1]];
         int val = -1;
         if (cache.find(location + 0x01000100) == cache.end())
         {
             if (p0[0] + 1 < grid.size() && grid[p0[0] + 1][p0[1]] != -1 && grid[p1[0] + 1][p1[1]] != -1)
             {
-                cherryPickup(grid, {p0[0] + 1, p0[1]}, {p1[0] + 1, p1[1]}, location + 0x01000100);
+                cherryPickup(grid, {p0[0] + 1, p0[1]}, {p1[0] + 1, p1[1]}, location + 0x01000100, waste);
             }
             else
             {
@@ -28,7 +34,7 @@ private:
         {
             if (p1[1] + 1 < grid.size() && grid[p0[0]][p0[1] + 1] != -1 && grid[p1[0]][p1[1] + 1] != -1)
             {
-                cherryPickup(grid, {p0[0], p0[1] + 1}, {p1[0], p1[1] + 1}, location + 0x00010001);
+                cherryPickup(grid, {p0[0], p0[1] + 1}, {p1[0], p1[1] + 1}, location + 0x00010001, waste);
             }
             else
             {
@@ -40,7 +46,7 @@ private:
         {
             if (p0[0] + 1 < grid.size() && p1[1] + 1 < grid.size() && grid[p0[0] + 1][p0[1]] != -1 && grid[p1[0]][p1[1] + 1] != -1)
             {
-                cherryPickup(grid, {p0[0] + 1, p0[1]}, {p1[0], p1[1] + 1}, location + 0x01000001);
+                cherryPickup(grid, {p0[0] + 1, p0[1]}, {p1[0], p1[1] + 1}, location + 0x01000001, waste);
             }
             else
             {
@@ -52,11 +58,11 @@ private:
         {
             if (p0[1] + 1 == p1[1] && p1[0] + 1 == p0[0] && grid[p0[0]][p1[1]] != -1)
             {
-                cherryPickup(grid, {p0[0], p1[1]}, location + 0x0010100);
+                cherryPickup(grid, {p0[0], p1[1]}, location + 0x00010100, waste + 1);
             }
             else if (grid[p0[0]][p0[1] + 1] != -1 && grid[p1[0] + 1][p1[1]] != -1)
             {
-                cherryPickup(grid, {p0[0], p0[1] + 1}, {p1[0] + 1, p1[1]}, location + 0x00010100);
+                cherryPickup(grid, {p0[0], p0[1] + 1}, {p1[0] + 1, p1[1]}, location + 0x00010100, waste);
             }
             else
             {
@@ -65,16 +71,34 @@ private:
         }
         val = max(val, cache[location + 0x00010100]);
         cache[location] = val == -1 ? -1 : grid[p0[0]][p0[1]] + grid[p1[0]][p1[1]] + val;
+        tolerance = min(tolerance, (int)grid.size() * 4 - 2 - cache[location]);
     }
 
-    void cherryPickup(vector<vector<int>> &grid, vector<int> p, unsigned int location)
+    void cherryPickup(vector<vector<int>> &grid, vector<int> p, unsigned int location, int waste)
     {
+        if (waste > tolerance)
+        {
+            return;
+        }
+        waste += 1 - grid[p[0]][p[1]];
         int val = -1;
+        if (cache.find(location + 0x01000001) == cache.end())
+        {
+            if (p[0] + 1 < grid.size() && grid[p[0] + 1][p[1]] != -1 && p[1] + 1 < grid.size() && grid[p[0]][p[1] + 1] != -1)
+            {
+                cherryPickup(grid, {p[0] + 1, p[1]}, {p[0], p[1] + 1}, location + 0x01000001, waste);
+            }
+            else
+            {
+                cache[location + 0x01000001] = -1;
+            }
+        }
+        val = max(val, cache[location + 0x01000001]);
         if (cache.find(location + 0x01000100) == cache.end())
         {
             if (p[0] + 1 < grid.size() && grid[p[0] + 1][p[1]] != -1)
             {
-                cherryPickup(grid, {p[0] + 1, p[1]}, location + 0x01000100);
+                cherryPickup(grid, {p[0] + 1, p[1]}, location + 0x01000100, waste + 1);
             }
             else
             {
@@ -86,7 +110,7 @@ private:
         {
             if (p[1] + 1 < grid.size() && grid[p[0]][p[1] + 1] != -1)
             {
-                cherryPickup(grid, {p[0], p[1] + 1}, location + 0x00010001);
+                cherryPickup(grid, {p[0], p[1] + 1}, location + 0x00010001, waste + 1);
             }
             else
             {
@@ -94,30 +118,19 @@ private:
             }
         }
         val = max(val, cache[location + 0x00010001]);
-        if (cache.find(location + 0x01000001) == cache.end())
-        {
-            if (p[0] + 1 < grid.size() && grid[p[0] + 1][p[1]] != -1 && p[1] + 1 < grid.size() && grid[p[0]][p[1] + 1] != -1)
-            {
-                cherryPickup(grid, {p[0] + 1, p[1]}, {p[0], p[1] + 1}, location + 0x01000001);
-            }
-            else
-            {
-                cache[location + 0x01000001] = -1;
-            }
-        }
-        val = max(val, cache[location + 0x01000001]);
         cache[location] = val == -1 ? -1 : grid[p[0]][p[1]] + val;
+        tolerance = min(tolerance, (int)grid.size() * 4 - 2 - cache[location]);
     }
 
 public:
     int cherryPickup(vector<vector<int>> &grid)
     {
-        if (grid.size() < 2)
+        if (grid.size() == 1)
         {
-            return grid[0][0] == 1 ? 1 : 0;
+            return grid[0][0];
         }
         cache[(grid.size() - 1) * 0x01010101] = grid[grid.size() - 1][grid.size() - 1];
-        cherryPickup(grid, {0, 0}, 0);
+        cherryPickup(grid, {0, 0}, 0, 1 - grid[0][0]);
         return cache[0] == -1 ? 0 : cache[0];
     }
 };
